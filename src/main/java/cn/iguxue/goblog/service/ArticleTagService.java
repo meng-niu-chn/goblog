482e889cc8c7bba7d8e85d37cd379585ef793a80
niumeng <niu6364@gmail.com>
2019-08-12 14:26:46 +0800

    修改注释

--- src/main/java/cn/iguxue/goblog/service/ArticleTagService.java
+++ src/main/java/cn/iguxue/goblog/service/ArticleTagService.java
@@ -9,39 +9,26 @@ import java.util.List;
 public interface ArticleTagService {
     /**
      * 根据文章ID查询文章+标签数据
-     *
-     * @param articleId
-     * @return
      */
     List<Tag> getByArticleId(Long articleId);
 
     /**
      * 根据标签ID查询文章+标签数据
-     *
-     * @param tagId
-     * @return
      */
     List<ArticleTag> getByTagId(Long tagId);
 
     /**
      * 根据文章ID删除
-     *
-     * @param id
      */
     void deleteByArticleId(Long id);
 
     /**
      * 根据标签ID删除
-     *
-     * @param id
      */
     void deleteByTagId(Long id);
 
     /**
      * 根据标签名称查询关联的文章
-     *
-     * @param tag
-     * @return
      */
     List<Article> getByTagName(String tag);
 
